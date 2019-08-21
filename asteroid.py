d1e88d27754024c8f753a42a5234a089d6d5b7ac
cwkreid19141 <student+19141@codingwithkids.com>
2019-08-21 15:55:17 -0700

    finished coding asteroids

--- asteroid.py
+++ asteroid.py
@@ -19,4 +19,8 @@ class Asteroid(pygame.sprite.Sprite):
         if self.rect.left < 0 or self.rect.right > screen_info.current_w:
             self.speed[0] *= -1
             self.image = pygame.transform.flip(self.image, True, False)
-            self.rect.move_ip(self.speed(0))
+            self.rect.move_ip(self.speed[0], 0)
+        if self.rect.top < 0 or self.rect.bottom > screen_info.current_w:
+            self.speed[1] *= -1
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.move_ip(0, self.speed[1])
